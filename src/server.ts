592f4f5539f7cd62a0d57df2943d1ff764595a03
Maicon-Mattos-de-Oliveira <maiconmattos9@gmail.com>
2025-05-29 13:11:36 -0300

    adicionado swagger com cadastro de filmes

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,14 @@
 import express from "express";
 import { PrismaClient } from "@prisma/client";
+import swaggerUi from "swagger-ui-express";
+import swaggerDocument from "../swagger.json";
 
 const port = 3000;
 const app = express();
 const prisma = new PrismaClient();
 
 app.use(express.json());
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // GET /movies – Lista todos os filmes
 app.get("/movies", async (_, res) => {
